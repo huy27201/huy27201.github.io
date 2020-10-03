5a4df64eb9918f5f288509e272fe9cd3fae1dcb0
huy27201 <61152052+huy27201@users.noreply.github.com>
2020-10-04 01:13:50 +0700

    Fixed

--- ITACWheel/Clock.js
+++ ITACWheel/Clock.js
@@ -1,55 +1,87 @@
-
 var present;
 var random = 0;
 var temp;
+var checkCharacter, checkCorrectCode, checkUsedCode = Array(100).fill(true);
+console.log(checkUsedCode);
+var code = [];
+    fetch('Code.txt')
+      .then(response => response.text())
+      .then(data => {
+          code = data.split('\n');
+      });
 function check() {
     swal({
-        text: 'Type the code (10 characters)',
+        text: 'Nhập mã 10 kí tự',
         content: "input",
         button: {
-          text: "Check",
+          text: "OK",
         },
     })
     .then(name => {
         if (name.length == 0) {
-            swal("Unsuccessful","You must type the code!","error");
+            swal("Không có mã!","Bạn phải nhập mã.","error");
         }
         else if (name.length != 10) {
-            swal("Unsuccessful!","Unavailable code!","error");
+            swal("Mã không tồn tại!","Mã bạn vừa nhập không tồn tại.","error");
         }
         else  {
-            var value = Math.floor(Math.random() * 100); 
-            if (value == 0)
+            checkCorrectCode = false;
+            for (var i = 0; i < 99; i++)
             {
-                temp = 130;
-                present = "Dây đeo Black";
-            } 
-            else if (value <= 8 && value >= 1) {
-                temp = 290;
-                present = "Dây đeo Golden";
-            }  
-            else if (value <= 34) {
-                let randomPiece = Math.floor(Math.random() * 10);
-                if (randomPiece < 4) temp = 50;
-                else temp = 250;
-                present = "Huy hiệu ITAC";
-            } 
-            else if (value <= 60) {
-                let randomPiece = Math.floor(Math.random() * 10);
-                if (randomPiece < 4) temp = 10;
-                else temp = 170;
-                present = "Phiếu ưu đãi 20%";
-            } 
-            else {
-                let randomPiece = Math.floor(Math.random() * 10);
-                if (randomPiece < 3) temp = 90;
-                else if (randomPiece < 6) temp = 210;
-                else temp = 330;
-                present = "Huy hiệu K20";
-            } 
-            random = random + temp + 3600;
-            document.getElementById("circle").style.transform=`rotate(${-random}deg)`;
-            random -= temp;
+                checkCharacter = true;
+                for (var j = 0; j < 10; j++)
+                {
+                    if (name[j] != code[i][j]) {
+                        checkCharacter = false;
+                        break;
+                    } 
+                }
+                if (checkCharacter == true) {
+                    checkCorrectCode = true;
+                    if (checkUsedCode[i] == true) {
+                        checkUsedCode[i] = false;
+                        var value = Math.floor(Math.random() * 100); 
+                        if (value == 0)
+                        {
+                            temp = 130;
+                            present = "Dây đeo Black";
+                        } 
+                        else if (value <= 8 && value >= 1) {
+                            temp = 290;
+                            present = "Dây đeo Golden";
+                        }  
+                        else if (value <= 34) {
+                            let randomPiece = Math.floor(Math.random() * 10);
+                            if (randomPiece < 4) temp = 50;
+                            else temp = 250;
+                            present = "Huy hiệu ITAC";
+                        } 
+                        else if (value <= 60) {
+                            let randomPiece = Math.floor(Math.random() * 10);
+                            if (randomPiece < 4) temp = 10;
+                            else temp = 170;
+                            present = "Phiếu ưu đãi 20%";
+                        } 
+                        else {
+                            let randomPiece = Math.floor(Math.random() * 10);
+                            if (randomPiece < 3) temp = 90;
+                            else if (randomPiece < 6) temp = 210;
+                            else temp = 330;
+                            present = "Huy hiệu K20";
+                        } 
+                        document.getElementById("layer").style.zIndex = "999";
+                        random = random + temp + 3600;
+                        document.getElementById("circle").style.transform=`rotate(${-random}deg)`;
+                        random -= temp;
+                        setTimeout(() => { 
+                            swal("Chúc mừng!", "Bạn đã trúng " + present + "!", "success");
+                            document.getElementById("layer").style.zIndex = "-1";
+                        }, 11000);
+                    }
+                    else swal("Không hợp lệ!","Bạn đã sử dụng mã này rồi!","error");
+                }
+            }
+            if (checkCorrectCode == false) swal("Mã không tồn tại!","Mã bạn vừa nhập không tồn tại.","error");
         }
     });
 }
