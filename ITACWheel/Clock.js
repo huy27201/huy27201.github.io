35b20460b22da777ea924b98549b7e888280e638
huy27201 <61152052+huy27201@users.noreply.github.com>
2020-10-03 21:51:10 +0700

    Fixed

--- ITACWheel/Clock.js
+++ ITACWheel/Clock.js
@@ -1,6 +1,7 @@
 
 var present;
 var random = 0;
+var temp;
 function check() {
     swal({
         text: 'Type the code (10 characters)',
@@ -10,28 +11,48 @@ function check() {
         },
     })
     .then(name => {
-        if ((name.length == 10) && (name != null))  {
-            var value = Math.floor((Math.random() * 3600) + 3600); 
-            random += value;
-            document.getElementById("circle").style.transform=`rotate(${-random}deg)`;
-            //setTimeout(remove, 10000);
+        if (name.length == 0) {
+            swal("Unsuccessful","You must type the code!","error");
         }
-        //    animation();  
-        else {
-            swal("Unsuccessful","The code is incorrect!","error");
+        else if (name.length != 10) {
+            swal("Unsuccessful!","Unavailable code!","error");
+        }
+        else  {
+            var value = Math.floor(Math.random() * 100); 
+            if (value == 0)
+            {
+                temp = 130;
+                present = "Dây đeo Black";
+            } 
+            else if (value <= 8 && value >= 1) {
+                temp = 290;
+                present = "Dây đeo Golden";
+            }  
+            else if (value <= 34) {
+                let randomPiece = Math.floor(Math.random() * 10);
+                if (randomPiece < 4) temp = 50;
+                else temp = 250;
+                present = "Huy hiệu ITAC";
+            } 
+            else if (value <= 60) {
+                let randomPiece = Math.floor(Math.random() * 10);
+                if (randomPiece < 4) temp = 10;
+                else temp = 170;
+                present = "Phiếu ưu đãi 20%";
+            } 
+            else {
+                let randomPiece = Math.floor(Math.random() * 10);
+                if (randomPiece < 3) temp = 90;
+                else if (randomPiece < 6) temp = 210;
+                else temp = 330;
+                present = "Huy hiệu K20";
+            } 
+            random = random + temp + 3600;
+            document.getElementById("circle").style.transform=`rotate(${-random}deg)`;
+            random -= temp;
         }
     });
 }
 document.getElementById("circle").addEventListener("click", () => {
     check();
-    
 });
-function animation() {
-    //document.getElementById("circle").classList.add("animate");
-    setTimeout(remove, 10000);
-}
-function remove() {
-    document.getElementById("circle").style.transition="0s";
-    document.getElementById("circle").style.transform="rotate(0deg)";
-    //document.getElementById("circle").classList.remove("animate");
-}
